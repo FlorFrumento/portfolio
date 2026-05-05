9ca3cd7cb865f2afb46f84cbde6fbcee2772a846
Florencia Frumento <florencia.frumento@gmail.com>
2026-05-05 11:12:10 -0300

    Refine hero gradient and header behavior

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const externalProjectLinks = document.querySelectorAll('.project-content a[href="#"]');
+const siteHeader = document.querySelector(".site-header");
+
+const updateHeaderState = () => {
+  siteHeader?.classList.toggle("is-scrolled", window.scrollY > 12);
+};
 
 externalProjectLinks.forEach((link) => {
   link.addEventListener("click", (event) => {
     event.preventDefault();
   });
 });
+
+updateHeaderState();
+window.addEventListener("scroll", updateHeaderState, { passive: true });
