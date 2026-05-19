113f92285818f33a7350999ac9f1c3776976721b
Florencia Frumento <florencia.frumento@gmail.com>
2026-05-19 20:45:39 -0300

    Add case study pages

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        bannerNubecommerce: resolve(__dirname, "casos/banner-nubecommerce.html"),
+        ctaMigrarTienda: resolve(__dirname, "casos/cta-migrar-tienda.html"),
+        recursosDescargables: resolve(__dirname, "casos/recursos-descargables.html")
+      }
+    }
+  }
+});
