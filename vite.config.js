7e9f6268689df4e39f29aab6c4ae3c31fcfb8e93
Florencia Frumento <florencia.frumento@gmail.com>
2026-05-26 16:41:14 -0300

    Add Ristretto case study and update site navigation

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         bannerNubecommerce: resolve(__dirname, "casos/banner-nubecommerce.html"),
+        ristretto: resolve(__dirname, "casos/ristretto.html"),
         ctaMigrarTienda: resolve(__dirname, "casos/cta-migrar-tienda.html"),
         recursosDescargables: resolve(__dirname, "casos/recursos-descargables.html")
       }
